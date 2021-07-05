e0a12574ea8b71c0e4b03324ce37b90fcb44ccee
Connor <73814263+c1625032@users.noreply.github.com>
2021-07-05 09:14:38 +0100

    Update 06_adding_repeat_information.sh

--- 06_adding_repeat_information.sh
+++ 06_adding_repeat_information.sh
@@ -5,34 +5,34 @@ module load bedtools
 
 # Calling bedtools intersect function and specifying it should provide all information from the original files 
 bedtools intersect -wa -wb \
--a /scratch/c.c1625032/dissertation/output/low_coverage_regions/mean_low_coverage_genes \
--b /scratch/c.c1625032/dissertation/resources/ucsc/selectionRepeatMasker | \
+-a <PATH>/mean_low_coverage_genes \
+-b selectionRepeatMasker | \
 # Keeping selected columns
-cut -f 1,2,3,4,5,6,7,12,13,14 > /scratch/c.c1625032/dissertation/output/repeatIntersect/genes_mean_repeat_intersect
+cut -f 1,2,3,4,5,6,7,12,13,14 > <PATH>/genes_mean_repeat_intersect
 
 ### Pct coverage genes	intersect
 
 # Calling bedtools intersect function and specifying it should provide all information from the original files
 bedtools intersect -wa -wb \
--a /scratch/c.c1625032/dissertation/output/low_coverage_regions/pct_low_coverage_genes \
--b /scratch/c.c1625032/dissertation/resources/ucsc/selectionRepeatMasker | \
+-a <PATH>/pct_low_coverage_genes \
+-b selectionRepeatMasker | \
 # Keeping selected columns
-cut -f 1,2,3,4,5,6,7,12,13,14 > /scratch/c.c1625032/dissertation/output/repeatIntersect/genes_pct_repeat_intersect
+cut -f 1,2,3,4,5,6,7,12,13,14 > <PATH>/genes_pct_repeat_intersect
 
 ### Mean coverage exons	intersect
 
 # Calling bedtools intersect function and specifying it should provide all information from the original files
 bedtools intersect -wa -wb \
--a /scratch/c.c1625032/dissertation/output/low_coverage_regions/mean_low_coverage_exons \
--b /scratch/c.c1625032/dissertation/resources/ucsc/selectionRepeatMasker | \
+-a <PATH>/mean_low_coverage_exons \
+-b selectionRepeatMasker | \
 # Keeping selected columns
-cut -f 1,2,3,4,5,6,7,8,13,14,15 > /scratch/c.c1625032/dissertation/output/repeatIntersect/exons_mean_repeat_intersect
+cut -f 1,2,3,4,5,6,7,8,13,14,15 > <PATH>/exons_mean_repeat_intersect
 
 ### Pct coverage exons intersect
 
 # Calling bedtools intersect function and specifying it should provide all information from the original files
 bedtools intersect -wa -wb \
--a /scratch/c.c1625032/dissertation/output/low_coverage_regions/pct_low_coverage_exons \
--b /scratch/c.c1625032/dissertation/resources/ucsc/selectionRepeatMasker | \
+-a <PATH>/pct_low_coverage_exons \
+-b selectionRepeatMasker | \
 # Keeping selected columns
-cut -f 1,2,3,4,5,6,7,8,13,14,15 > /scratch/c.c1625032/dissertation/output/repeatIntersect/exons_pct_repeat_intersect
+cut -f 1,2,3,4,5,6,7,8,13,14,15 > <PATH>/exons_pct_repeat_intersect
