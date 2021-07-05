d084737eb1cc731417f937bc9b2a72be0ec1f958
Connor <73814263+c1625032@users.noreply.github.com>
2021-07-05 09:04:48 +0100

    Update 01_removing_failed_samples.py

--- 01_removing_failed_samples.py
+++ 01_removing_failed_samples.py
@@ -8,7 +8,7 @@ import pandas as pd
 #print(os.getcwd())
 
 # Path to data directory is stored in a variable so it is easier to refer to later.
-dataDirectory = "/scratch/c.c1625032/dissertation/data"
+dataDirectory = "<path>"
 
 # Creating a list containing the names of all files within the data directory so they can be called easier.
 fileNames = os.listdir(dataDirectory)
