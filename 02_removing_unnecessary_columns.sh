4bd1f16a49a3e5a6b564552ff42b126b3be1529d
Connor <73814263+c1625032@users.noreply.github.com>
2021-07-05 08:56:55 +0100

    Delete 02_removing_unnecessary_columns.sh
    Added more robust script.

--- 02_removing_unnecessary_columns.sh
+++
@@ -1,9 +0,0 @@
-# Entering data directory 
-cd /scratch/c.c1625032/dissertation/data/
-
-# for loop to iterate over each of the bed files in directory
-for i in *.bed; do
-	# awk to print the columns of interest in each bed file and create a temp  
-	awk -F "\t" '{ print $1, $2, $3, $5, $7, $12, $13 }' "$i" > "temp_file"
-	# renaming each of the temp files using their .bed file names 
-	mv temp_file "$i"; done
