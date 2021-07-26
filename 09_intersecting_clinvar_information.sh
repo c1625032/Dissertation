f7463d00852260b4f3f3383de3c98254ea1a9576
Connor <73814263+c1625032@users.noreply.github.com>
2021-07-26 07:14:20 +0100

    Update 09_intersecting_clinvar_information.sh

--- 09_intersecting_clinvar_information.sh
+++ 09_intersecting_clinvar_information.sh
@@ -5,32 +5,32 @@ module load bedtools
 
 # Calling bedtools intersect function and specifying it should provide all information from the original files
 bedtools intersect -wa -wb \
--a /scratch/c.c1625032/dissertation/output/low_coverage_outputs/04-repeat-annotation/genes_mean_repeat_annotation \
--b /scratch/c.c1625032/dissertation/resources/clinvar/pathogenic-only-clinvar.vcf | \
+-a <PATH>/genes_mean_repeat_annotation \
+-b <PATH>/pathogenic-only-clinvar.vcf | \
 cut -f2 | \
-awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' > /scratch/c.c1625032/dissertation/output/low_coverage_outputs/05-clinvar-annotation/genes_mean_repeat_clinvar_intersection
+awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' > <PATH>/genes_mean_repeat_clinvar_intersection
 
 ### Pct coverage genes intersect
 # Calling bedtools intersect function and specifying it should provide all information from the original files
 bedtools intersect -wa -wb \
--a /scratch/c.c1625032/dissertation/output/low_coverage_outputs/04-repeat-annotation/genes_pct_repeat_annotation \
--b /scratch/c.c1625032/dissertation/resources/clinvar/pathogenic-only-clinvar.vcf | \
+-a <PATH>/genes_pct_repeat_annotation \
+-b <PATH>/pathogenic-only-clinvar.vcf | \
 cut -f2 | \
-awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' > /scratch/c.c1625032/dissertation/output/low_coverage_outputs/05-clinvar-annotation/genes_pct_repeat_clinvar_intersection
+awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' > <PATH>/genes_pct_repeat_clinvar_intersection
 
 ### Mean coverage exons intersect
 # Calling bedtools intersect function and specifying it should provide all information from the original files
 bedtools intersect -wa -wb \
--a /scratch/c.c1625032/dissertation/output/low_coverage_outputs/04-repeat-annotation/exons_mean_repeat_annotation \
--b /scratch/c.c1625032/dissertation/resources/clinvar/pathogenic-only-clinvar.vcf | \
+-a <PATH>/exons_mean_repeat_annotation \
+-b <PATH>/pathogenic-only-clinvar.vcf | \
 cut -f2 | \
-awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' > /scratch/c.c1625032/dissertation/output/low_coverage_outputs/05-clinvar-annotation/exons_mean_repeat_clinvar_intersection
+awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' > <PATH>/exons_mean_repeat_clinvar_intersection
 
 ### Pct coverage exons intersect
 # Calling bedtools intersect function and specifying it should provide all information from the original files
 bedtools intersect -wa -wb \
--a /scratch/c.c1625032/dissertation/output/low_coverage_outputs/04-repeat-annotation/exons_pct_repeat_annotation \
--b /scratch/c.c1625032/dissertation/resources/clinvar/pathogenic-only-clinvar.vcf | \
+-a <PATH>/exons_pct_repeat_annotation \
+-b <PATH>/pathogenic-only-clinvar.vcf | \
 cut -f2 | \
-awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' > /scratch/c.c1625032/dissertation/output/low_coverage_outputs/05-clinvar-annotation/exons_pct_repeat_clinvar_intersection
+awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' > <PATH>/exons_pct_repeat_clinvar_intersection
 
