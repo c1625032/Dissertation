7a587f87d33a006e3aac32e27882077387df684b
Connor <73814263+c1625032@users.noreply.github.com>
2021-07-26 07:18:40 +0100

    Update 07_adding_repeat_information.sh

--- 07_adding_repeat_information.sh
+++ 07_adding_repeat_information.sh
@@ -2,16 +2,16 @@
 module load bedtools
 
 # Moving into output directory
-cd /scratch/c.c1625032/dissertation/output/all_regions_output/04-repeat-annotation/
+cd <PATH>/output/all_regions_output/04-repeat-annotation/
 
 ### Performing intersect
 
 # Calling bedtools intersect function and specifying it should provide all information from the original files
 bedtools intersect -wa -wb \
--a /scratch/c.c1625032/dissertation/output/all_regions_output/03-calculating_average_coverage/total_dataframe \
--b /scratch/c.c1625032/dissertation/resources/ucsc/selectionRepeatMasker | \
+-a <PATH>/output/all_regions_output/03-calculating_average_coverage/total_dataframe \
+-b <PATH>/resources/ucsc/selectionRepeatMasker | \
 # Keeping selected columns
-cut -f 1,2,3,4,5,6,7,8,9,15 > /scratch/c.c1625032/dissertation/output/all_regions_output/04-repeat-annotation/total_dataframe_repeat_annotation
+cut -f 1,2,3,4,5,6,7,8,9,15 > <PATH>/output/all_regions_output/04-repeat-annotation/total_dataframe_repeat_annotation
 
 # Removing duplicate lines following sorting
 sort -k1,1 -k2,2 -k3,3 total_dataframe_repeat_annotation | uniq > rmdup
