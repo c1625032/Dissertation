0aaf7b7bc16369193f96cdddeecb67c4b4931d65
Connor <73814263+c1625032@users.noreply.github.com>
2021-08-13 10:46:55 +0100

    Update 08_merging_repeat_information.py

--- 08_merging_repeat_information.py
+++ 08_merging_repeat_information.py
@@ -1,18 +1,18 @@
 # Importing modules.
-# os is used to perform different opperating system tasks, sys is used to manipulate the run time environment.
-import os, sys
-# Pandas is a data frame manipulation module and here the as is used to create a shortcut name pd so calling the modules functions is$
+# os is used to perform different opperating system tasks.
+import os
+# Pandas is a data frame manipulation module, used 'as' to refer to pandas as 'pd' later in scripts.
 import pandas as pd
 
-# Reading in file
-file = pd.read_table("<PATH>/output/all_regions_output/04-repeat-annotation/total_dataframe_repeat_annotation", names=["Chr", "Start", "End", "Strand", "Gene_ID", "Gene_Name", "Exon(s)", "Mean_Coverage", "Percent_Coverage", "Repeat_Class"])
+# Reading in data frame file. 
+file = pd.read_table("<PATH_to_output>/total_dataframe_repeat_annotation", names=["Chr", "Start", "End", "Strand", "Gene_ID", "Gene_Name", "Exon(s)", "Mean_Coverage", "Percent_Coverage", "Repeat_Class"])
 
-# Grouping repeat name column if all other columns match
+# Grouping repeat name column if all other columns match.
 file = file.groupby(by=['Chr', 'Start', 'End', 'Strand', 'Gene_ID', 'Gene_Name', 'Exon(s)', 'Mean_Coverage', 'Percent_Coverage'], as_index=False).agg({'Repeat_Class': '/'.join})
 
-# Rearranging columns
+# Rearranging columns.
 file = file.reindex(columns=["Chr", "Start", "End", "Strand", "Gene_ID", "Gene_Name", "Exon(s)", "Mean_Coverage", "Percent_Coverage", "Repeat_Class"])
 
-# Exporting file
-file.to_csv("<PATH>/output/all_regions_output/04-repeat-annotation/total_dataframe_repeat_annotation",header=True, sep='\t', index=False)
+# Exporting file.
+file.to_csv("<PATH_to_output_file>/total_dataframe_repeat_annotation",header=True, sep='\t', index=False)
 
