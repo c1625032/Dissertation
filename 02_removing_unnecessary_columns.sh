b3d2b2f7096772d4adcde764ad0a3551463562a0
Connor <73814263+c1625032@users.noreply.github.com>
2021-08-13 09:51:18 +0100

    Update 02_removing_unnecessary_columns.sh

--- 02_removing_unnecessary_columns.sh
+++ 02_removing_unnecessary_columns.sh
@@ -1,7 +1,7 @@
-# Entering data directory 
+# Entering data directory. 
 cd <PATH>
 
-# for loop to iterate over each of the bed files in directory
+# for loop to iterate over each of the bed files in data directory.
 for i in *.bed; do
 	# cut to select specific columns in each bed file and create a temp file
 	cut -f 1,2,3,5,7,12,13 "$i" > "temp_file"
