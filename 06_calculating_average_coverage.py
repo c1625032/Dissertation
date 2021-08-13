c32374f89d3d78e6d7a22edbc93fb73b42b43ee4
Connor <73814263+c1625032@users.noreply.github.com>
2021-08-13 10:40:29 +0100

    Update 06_calculating_average_coverage.py

--- 06_calculating_average_coverage.py
+++ 06_calculating_average_coverage.py
@@ -1,43 +1,40 @@
 # Importing modules.
-# os is used to perform different opperating system tasks, sys is used to manipulate the run time environment.
-import os, sys
-# Pandas is a data frame manipulation module and here the as is used to create a shortcut name pd so calling the modules functions is easier.
+# os is used to perform different opperating system tasks.
+import os
+# Pandas is a data frame manipulation module, used 'as' to refer to pandas as 'pd' later in scripts.
 import pandas as pd
 
-# User needs to enter path to output directory. Can use the following commented out command.
-#print(os.getcwd())
-
-# Path to output directory is stored in a variable so it is easier to refer to later.
-outputDirectory = "<PATH>/02-merging-bed-files"
+# Path to output directory is stored as an object.
+outputDirectory = "<PATH_to_output_directory>"
 
 ### Mean coverage 
 
-# Reading in mean coverage file
+# Reading in mean coverage file.
 mean = pd.read_table(os.path.join(outputDirectory, "exons_mean_coverage_dataframe.bed"))
 
-# Calculating mean coverage values by creating a new column
+# Calculating mean coverage values by creating a new column.
 mean['Mean_Coverage'] = mean.iloc[:, 7:168].mean(axis=1)
 
-# Selecting columns for final data drame
+# Selecting columns for final data frame.
 mean = mean.iloc[:, [0] + [1] + [2] + [3] + [4] + [5] + [6] + [-1]]
 
 ### Pct Coverage
 
-# Reading in above 15 pct file
+# Reading in above 15 pct file.
 pct = pd.read_table(os.path.join(outputDirectory, "exons_pct_above_15_dataframe.bed"))
 
-# Calculating mean coverage values by creating a new column
+# Calculating mean above 15 pct values by creating a new column.
 pct['Coverage_above_15_pct'] = pct.iloc[:, 7:168].mean(axis=1)
 
-# Selecting columns for final data drame
+# Selecting columns for final data frame.
 pct = pct.iloc[:, [1] + [-1]]
 
 ### Merging dataframes
 
-# Performing merge
+# Merging files
 output = mean.merge(pct, how='left', on='Start')
 
-# Exporting dataframe
-output.to_csv("<PATH>/output/all_regions_output/03-calculating_average_coverage/total_dataframe", header=True, sep='\t', index=False)
+# Exporting dataframe.
+output.to_csv("<PATH_to_output_directory>/03-calculating_average_coverage/total_dataframe", header=True, sep='\t', index=False)
 
 print('Done')
