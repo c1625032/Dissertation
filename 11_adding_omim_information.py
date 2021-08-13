d2445519f7f53bf2339a2cfd4f99f552ffcf4865
Connor <73814263+c1625032@users.noreply.github.com>
2021-08-13 11:18:50 +0100

    Update 11_adding_omim_information.py

--- 11_adding_omim_information.py
+++ 11_adding_omim_information.py
@@ -1,34 +1,28 @@
 # Importing modules.
-# os is used to perform different opperating system tasks, sys is used to manipulate the run time environment.
-import os, sys
-# Pandas is a data frame manipulation module and here the as is used to create a shortcut name pd so calling the modules functions is easier.
+# os is used to perform different opperating system tasks.
+import os
+# Pandas is a data frame manipulation module, used 'as' to refer to pandas as 'pd' later in scripts.
 import pandas as pd
 
-# User needs to enter path to output directory. Can use the following commented out command.
-#print(os.getcwd())
-
-# Path to clinvar directory is stored in a variable so it is easier to refer to later.
+# Path to clinvar directory is stored as an object.
 clinvarDirectory = "<PATH>/output/all_regions_output/05-clinvar-annotation/"
 
 # Reading in omim file
-omim = pd.read_table("<PATH>/resources/omim/genemap2.txt")
+omim = pd.read_table("<PATH_to_resources_directory>/genemap2.txt")
 
-# Selecting appropriate columns
+# Selecting appropriate columns.
 omim = omim.iloc[ : , [10] + [12]]
 
-# Renaming columns
+# Renaming columns.
 omim.columns = ['Gene_ID', 'OMIM_Phenotypes']
 
-##### Exons
-
-### Mean coverage 
-# Reading in clinvar annotation file
+# Reading in clinvar annotation file.
 clinvar = pd.read_table(os.path.join(clinvarDirectory, "total_dataframe_repeat_clinvar_annotation"))
 
-# Performing merge
+# Performing merge.
 output = clinvar.merge(omim, how='left', on='Gene_ID')
 
 # Exporting low coverage genes dataframe
-output.to_csv("<PATH>/output/all_regions_output/06-omim-annotation/total_dataframe_repeat_clinvar_omim_annotation", header=True, sep='\t', index=False)
+output.to_csv("<PATH_to_output_directory>/total_dataframe_repeat_clinvar_omim_annotation", header=True, sep='\t', index=False)
 
 print('Done')
