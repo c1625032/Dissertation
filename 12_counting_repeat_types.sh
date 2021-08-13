f99d46f155fea510f469e6edfdb87c882072aba9
Connor <73814263+c1625032@users.noreply.github.com>
2021-08-13 11:58:34 +0100

    Update 12_counting_repeat_types.sh

--- 12_counting_repeat_types.sh
+++ 12_counting_repeat_types.sh
@@ -1,36 +1,30 @@
 ### Mean coverage exons
 
-# Entering direcory
-cd <PATH>/06-omim-annotation
+# Entering output direcory.
+cd <PATH>
 
-# Cutting the repeat columns
+# Cutting the repeat class column.
 cut -f10 exons_mean_repeat_clinvar_omim_annotation > mean_class
-cut -f11 exons_mean_repeat_clinvar_omim_annotation > mean_family
 
-# Removing header row
+# Removing header row.
 sed '1d' mean_class > class_tmp
-sed '1d' mean_family > family_tmp
 
-# Counting number of each type of repeat
+# Counting number of each type of repeat.
 awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' class_tmp > mean_class
-awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' family_tmp > mean_family
 
-# Removing spare files
-rm class_tmp family_tmp 
+# Removing spare file.
+rm class_tmp
 
 ### pct coverage exons
 
-# Cutting the repeat columns
+# Cutting the repeat class column.
 cut -f10 exons_pct_repeat_clinvar_omim_annotation > pct_class
-cut -f11 exons_pct_repeat_clinvar_omim_annotation > pct_family
 
-# Removing header row
+# Removing header row.
 sed '1d' pct_class > class_tmp
-sed '1d' pct_family > family_tmp
 
-# Counting number of each type of repeat
+# Counting number of each type of repeat.
 awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' class_tmp > pct_class
-awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' family_tmp > pct_family
 
-# Removing spare files
-rm class_tmp family_tmp
+# Removing spare file.
+rm class_tmp
