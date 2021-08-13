135a13e42878c05016505af5b8562073bc934456
Connor <73814263+c1625032@users.noreply.github.com>
2021-08-13 11:16:02 +0100

    Update 10_adding_clinvar_information.py

--- 10_adding_clinvar_information.py
+++ 10_adding_clinvar_information.py
@@ -1,28 +1,26 @@
 # Importing modules.
-# os is used to perform different opperating system tasks, sys is used to manipulate the run time environment.
-import os, sys
-# Pandas is a data frame manipulation module and here the as is used to create a shortcut name pd so calling the modules functions is easier.
+# os is used to perform different opperating system tasks.
+import os
+# Pandas is a data frame manipulation module, used 'as' to refer to pandas as 'pd' later in scripts.
 import pandas as pd
 
-# User needs to enter path to output directory. Can use the following commented out command.
-#print(os.getcwd())
 
-# Path to clinvar directory is stored in a variable so it is easier to refer to later.
-clinvarDirectory = "<PATH>/output/all_regions_output/05-clinvar-annotation"
+# Path to clinvar directory is stored as an object.
+clinvarDirectory = "<PATH_to_output_directory>/05-clinvar-annotation"
 
-# Path to repeat directory is stored in a variable so it is easier to refer to later.
-repeatDirectory = "<PATH>/output/all_regions_output/04-repeat-annotation"
+# Path to repeat directory is stored as an object.
+repeatDirectory = "<PATH_to_output_directory>/04-repeat-annotation"
  
-# Reading in repeat annotation file
+# Reading in repeat annotation file.
 repeat = pd.read_table(os.path.join(repeatDirectory, "total_dataframe_repeat_annotation"))
 
-# Reading in clinvar interection file
+# Reading in clinvar intersection file.
 clinvar = pd.read_table(os.path.join(clinvarDirectory, "total_dataframe_repeat_clinvar_intersection"), names=["Start", "Number_of_pathogenic_variants"])
 
-# Performing merge
+# Performing merge.
 output = repeat.merge(clinvar, how='left', on='Start')
 
-# Exporting low coverage genes dataframe
-output.to_csv("<PATH>/output/all_regions_output/05-clinvar-annotation/total_dataframe_repeat_clinvar_annotation", header=True, sep='\t', index=False)
+# Exporting file.
+output.to_csv("<PATH_to_output_directory>/total_dataframe_repeat_clinvar_annotation", header=True, sep='\t', index=False)
 
 print('Done')
