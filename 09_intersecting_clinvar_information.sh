507dfc54ffbbb28699e55a651e95537efde6296b
Connor <73814263+c1625032@users.noreply.github.com>
2021-08-13 11:02:52 +0100

    Update 09_intersecting_clinvar_information.sh

--- 09_intersecting_clinvar_information.sh
+++ 09_intersecting_clinvar_information.sh
@@ -1,12 +1,13 @@
-# Loading in approproate modules
+# Loading in approproate module.
 module load bedtools
 
-### Mean coverage exons intersect
-# Calling bedtools intersect function and specifying it should provide all information from the original files
+# Calling bedtools intersect function and specifying opton to keep all information from the original file and add intersecticing information from the gtf.
 bedtools intersect -wa -wb \
--a <PATH>/output/all_regions_output/04-repeat-annotation/total_dataframe_repeat_annotation \
--b <PATH>/resources/clinvar/pathogenic-only-clinvar.vcf | \
+-a <PATH_to_output_directory>/total_dataframe_repeat_annotation \
+-b <PATH_to_resources>/pathogenic-only-clinvar.vcf | \
+# Cutting the second column.
 cut -f2 | \
-awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' > <PATH>/all_regions_output/05-clinvar-annotation/total_dataframe_repeat_clinvar_intersection
+# Using awk command to count the number of duplicatea and present them in an array.
+awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' > <PATH_to_output_directory>/total_dataframe_repeat_clinvar_intersection
 
 
