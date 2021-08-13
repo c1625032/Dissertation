e6e093abf01f2eaa3e0d834e5ee6c55f130b2303
Connor <73814263+c1625032@users.noreply.github.com>
2021-08-13 11:48:48 +0100

    Update 09_intersecting_clinvar_information.sh

--- 09_intersecting_clinvar_information.sh
+++ 09_intersecting_clinvar_information.sh
@@ -3,34 +3,42 @@ module load bedtools
 
 ### Mean coverage genes intersect
 
-# Calling bedtools intersect function and specifying it should provide all information from the original files
+# Calling bedtools intersect function and specifying opton to keep all information from the original file and add intersecticing information from the vcf.
 bedtools intersect -wa -wb \
--a <PATH>/genes_mean_repeat_annotation \
--b <PATH>/pathogenic-only-clinvar.vcf | \
+-a <PATH_to_output_directory>/genes_mean_repeat_annotation \
+-b <PATH_to_resource_directory>/pathogenic-only-clinvar.vcf | \
+# Cutting the second column.
 cut -f2 | \
+# Using awk command to count the number of duplicatea and present them in an array.
 awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' > <PATH>/genes_mean_repeat_clinvar_intersection
 
 ### Pct coverage genes intersect
-# Calling bedtools intersect function and specifying it should provide all information from the original files
+# Calling bedtools intersect function and specifying opton to keep all information from the original file and add intersecticing information from the vcf.
 bedtools intersect -wa -wb \
--a <PATH>/genes_pct_repeat_annotation \
--b <PATH>/pathogenic-only-clinvar.vcf | \
+-a <PATH_to_output_directory>/genes_pct_repeat_annotation \
+-b <PATH_to_resource_directory>/pathogenic-only-clinvar.vcf | \
+# Cutting the second column.
 cut -f2 | \
+# Using awk command to count the number of duplicatea and present them in an array.
 awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' > <PATH>/genes_pct_repeat_clinvar_intersection
 
 ### Mean coverage exons intersect
-# Calling bedtools intersect function and specifying it should provide all information from the original files
+# Calling bedtools intersect function and specifying opton to keep all information from the original file and add intersecticing information from the vcf.
 bedtools intersect -wa -wb \
--a <PATH>/exons_mean_repeat_annotation \
--b <PATH>/pathogenic-only-clinvar.vcf | \
+-a <PATH_to_output_directory>/exons_mean_repeat_annotation \
+-b <PATH_to_resource_directory>/pathogenic-only-clinvar.vcf | \
+# Cutting the second column.
 cut -f2 | \
+# Using awk command to count the number of duplicatea and present them in an array.
 awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' > <PATH>/exons_mean_repeat_clinvar_intersection
 
 ### Pct coverage exons intersect
-# Calling bedtools intersect function and specifying it should provide all information from the original files
+# Calling bedtools intersect function and specifying opton to keep all information from the original file and add intersecticing information from the vcf.
 bedtools intersect -wa -wb \
--a <PATH>/exons_pct_repeat_annotation \
--b <PATH>/pathogenic-only-clinvar.vcf | \
+-a <PATH_to_output_directory>/exons_pct_repeat_annotation \
+-b <PATH_to_resource_directory>/pathogenic-only-clinvar.vcf | \
+# Cutting the second column.
 cut -f2 | \
+# Using awk command to count the number of duplicatea and present them in an array.
 awk 'BEGIN{FS=OFS="\t"} {arr[$0]++} END{for(i in arr){print i,arr[i]}}' > <PATH>/exons_pct_repeat_clinvar_intersection
 
